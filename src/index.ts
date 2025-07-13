3eb455d882e6e1437934ad0af20984e8665f6f1a
Emmo00 <nwaforemmanuel005@gmail.com>
2025-07-13 21:11:54 +0100

    feat: implement caching mechanism for Arweave requests and improve error handling

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
-import "dotenv/config"
-import { testConnectionToArweaveGateway } from "./utils/arweave"
-import { connectDB } from "./config/mongo"
-import { startServer } from "./config/server"
-import { fetchAndStoreMeters, updateMetersState } from "./jobs/meters"
-import { logMemoryStatistics } from "./utils/helpers"
+import "dotenv/config";
+import { testConnectionToArweaveGateway } from "./utils/arweave";
+import { connectDB } from "./config/mongo";
+import { startServer } from "./config/server";
+import { fetchAndStoreMeters, updateMetersState } from "./jobs/meters";
+import { logMemoryStatistics } from "./utils/helpers";
 
-console.log("Memory Stats:")
-console.log(process.memoryUsage())
-console.log("Starting application...")
+console.log("Memory Stats:");
+console.log(process.memoryUsage());
+console.log("Starting application...");
 // start server
 startServer()
-testConnectionToArweaveGateway().then(() => {
-  connectDB().then(() => {
-    logMemoryStatistics()
-    // fetchAndStoreMeters().then(() => {
-    //   console.log("Meters fetched and stored successfully")
-    //   // updateMetersState();
-    // })
+  .then(() => {
+    console.log("Server started successfully");
+    connectDB().then(() => {
+      logMemoryStatistics();
+    });
   })
-})
+  .catch((error) => {
+    console.error("Error starting server:", error);
+  });
+
+testConnectionToArweaveGateway().then(() => {});
